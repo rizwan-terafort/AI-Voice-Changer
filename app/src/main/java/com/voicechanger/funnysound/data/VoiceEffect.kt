c499aafb50fbbf48855f43c77a3885d138b515cf
Rizwan Khan <rizwan.khan@terafort.org>
2025-08-28 12:45:41 +0500

    voice effectss continue

---
+++ app/src/main/java/com/voicechanger/funnysound/data/VoiceEffect.kt
@@ -0,0 +1,9 @@
+package com.voicechanger.funnysound.data
+
+data class VoiceEffect(
+    val name: String,
+    val iconResId: Int,
+    val expandedText: String,
+    val expandedImageResId: Int
+)
+
